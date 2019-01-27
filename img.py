9fc9de67ec383f03f92b90a4450c856415be7677
Derek Rodriguez <dwrodri@g.clemson.edu>
2019-01-27 01:43:09 +0000

    did I not do this

--- img.py
+++ img.py
@@ -1,6 +1,28 @@
-from PIL import Image
+#! /usr/bin/python2
+import imageio
+import png
+import numpy as np
 
-img_file = Image.open("DA_MAP.png")
+# load image and delete alpha channel
+IMG = imageio.imread("DA_MAP.png")
+print IMG.shape
+IMG = np.delete(IMG,-1,axis=-1)
+print IMG.shape
 
-img_file.show()
+def update_pixel(x,y,r,g,b):
+    global IMG
+    temp = [r,g,b]
+    for i in xrange(len(temp)):
+        IMG[x,y,i] = temp[i]
 
+def main():
+    global IMG
+    lines = []
+    with open('/dev/stdin') as file_handle:
+        lines = file_handle.readlines()
+    for line in lines:
+        print line.strip()
+        
+   
+if __name__ == "__main__":
+    main() 
